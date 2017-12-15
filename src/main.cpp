dfe64622ba31f6a7c6624ad258050d914eda8218
GuilhermeHaetinger <ghaetinger@gmail.com>
2017-12-15 20:12:16 -0200

    options "added", 3 blocks a time now

--- src/main.cpp
+++ src/main.cpp
@@ -170,6 +170,7 @@ GLuint CreateGpuProgram(GLuint vertex_shader_id, GLuint fragment_shader_id); // 
 void PrintObjModelInfo(ObjModel*); // Função para debugging
 void playGame();
 int menu();
+void optionsMenu();
 
 // Declaração de funções auxiliares para renderizar texto dentro da janela
 // OpenGL. Estas funções estão definidas no arquivo "textrendering.cpp".
@@ -547,9 +548,18 @@ int main(int argc, char* argv[])
         case 0 :
             playGame();
             break;
+
+        case 1:
+          optionsMenu();
+          break;
+
         case 2:
             return 0;
             break;
+
+        default:
+          break;
+
 
 
         }
@@ -782,10 +792,89 @@ int menu()
 
     engine->stopAllSounds();
     engine->play2D("../../audio/initGame.wav", false);
+    enterPressed = false;
     return selectPos;
 
 }
 
+
+void optionsMenu()
+{
+    engine->play2D("../../audio/menuSong.wav", true);
+
+    while(!enterPressed){
+
+        glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        // "Pintamos" todos os pixels do framebuffer com a cor definida acima,
+        // e também resetamos todos os pixels do Z-buffer (depth buffer).
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+        glUseProgram(program_id);
+        float r = g_CameraDistance;
+        float y = r*sin(g_CameraPhi);
+        float z = r*cos(g_CameraPhi)*cos(g_CameraTheta);
+        float x = r*cos(g_CameraPhi)*sin(g_CameraTheta);
+
+        // Abaixo definimos as varáveis que efetivamente definem a câmera virtual.
+        // Veja slide 165 do documento "Aula_08_Sistemas_de_Coordenadas.pdf".
+        glm::vec4 camera_position_c  = glm::vec4(x,y,z,1.0f); // Ponto "c", centro da câmera
+        glm::vec4 camera_lookat_l    = glm::vec4(0.0f,0.0f,0.0f,1.0f); // Ponto "l", para onde a câmera (look-at) estará sempre olhando
+        glm::vec4 camera_view_vector = camera_lookat_l - camera_position_c; // Vetor "view", sentido para onde a câmera está virada
+        glm::vec4 camera_up_vector   = glm::vec4(0.0f,1.0f,0.0f,0.0f); // Vetor "up" fixado para apontar para o "céu" (eito Y global)
+
+        // Computamos a matriz "View" utilizando os parâmetros da câmera para
+        // definir o sistema de coordenadas da câmera.  Veja slide 169 do
+        // documento "Aula_08_Sistemas_de_Coordenadas.pdf".
+        glm::mat4 view = Matrix_Camera_View(camera_position_c, camera_view_vector, camera_up_vector);
+
+        // Agora computamos a matriz de Projeção.
+        glm::mat4 projection;
+
+        // Note que, no sistema de coordenadas da câmera, os planos near e far
+        // estão no sentido negativo! Veja slides 198-200 do documento
+        // "Aula_09_Projecoes.pdf".
+        float nearplane = -0.1f;  // Posição do "near plane"
+        float farplane  = -100.0f; // Posição do "far plane"
+
+        if (g_UsePerspectiveProjection)
+        {
+            // Projeção Perspectiva.
+            // Para definição do field of view (FOV), veja slide 234 do
+            // documento "Aula_09_Projecoes.pdf".
+            float field_of_view = 3.141592 / 3.0f;
+            projection = Matrix_Perspective(field_of_view, g_ScreenRatio, nearplane, farplane);
+        }
+        else
+        {
+            // Projeção Ortográfica.
+            // Para definição dos valores l, r, b, t ("left", "right", "bottom", "top"),
+            // veja slide 243 do documento "Aula_09_Projecoes.pdf".
+            // Para simular um "zoom" ortográfico, computamos o valor de "t"
+            // utilizando a variável g_CameraDistance.
+            float t = 1.5f*g_CameraDistance/2.5f;
+            float b = -t;
+            float r = t*g_ScreenRatio;
+            float l = -r;
+            projection = Matrix_Orthographic(l, r, b, t, nearplane, farplane);
+        }
+
+        TextRendering_PrintString(window, "Sometimes life gives you no options", -1.0f, 0.0f, 4.0f);
+
+        glfwSwapBuffers(window);
+
+
+        // Verificamos com o sistema operacional se houve alguma interação do
+        // usuário (teclado, mouse, ...). Caso positivo, as funções de callback
+        // definidas anteriormente usando glfwSet*Callback() serão chamadas
+        // pela biblioteca GLFW.
+        glfwPollEvents();
+    }
+
+    enterPressed = false;
+
+
+}
 
 void playGame()
 {
@@ -1062,6 +1151,8 @@ void playGame()
                         if(waveCounter>=0)
                         {
                             createRandomNextCube(&cubos, cubos[cubos.size()-1], cubeText + 9);
+                            createRandomNextCube(&cubos, cubos[cubos.size()-1], cubeText + 9);
+                            createRandomNextCube(&cubos, cubos[cubos.size()-1], cubeText + 9);
                         }
 
                         createEnemies(cubos.size(), &enemies, cubos[cubos.size()-1]);
@@ -1318,6 +1409,8 @@ void playGame()
         whileTime = (glfwGetTime() * 10) - actualSecond;
     }
     engine->stopAllSounds();
+    glfwSetWindowShouldClose(window, GL_FALSE);
+
 }
 
 void draw3Enemies(glm::vec3 cubeCenter, float cubeHeight, float cubeRadius)
